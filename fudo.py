e235f3d2cca664fc3aa4fbedad950f200b9954d9
Toni Korpela <toni@mussukka.org>
2015-04-25 21:01:23 +0300

    open files with "with"

--- fudo.py
+++ fudo.py
@@ -12,8 +12,8 @@ class Fudo:
         if hasattr(self, 'todoViewport') == False:
             self.todoViewport = self.builder.get_object("todoViewport")
         self.todoVBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=1)
-        f = open("lists/todo.json", "r")
-        data = json.loads(f.read())
+        with open("lists/todo.json", "r") as f:
+            data = json.loads(f.read())
         for fudo in data:
             button = Gtk.Button(fudo[0])
             button.connect("clicked", self.onToDoClick)
@@ -25,8 +25,8 @@ class Fudo:
         if hasattr(self, "doingViewport") == False:
             self.doingViewport = self.builder.get_object("doingViewport");
         self.doingVBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=1)
-        f = open("lists/doing.json", "r");
-        doing = json.loads(f.read())
+        with open("lists/doing.json", "r") as f:
+            doing = json.loads(f.read())
         for fudo in doing:
             button = Gtk.Button(fudo[0]);
             button.connect("clicked", self.onDoingClick);
@@ -38,8 +38,8 @@ class Fudo:
         if hasattr(self, "doneViewport") == False:
             self.doneViewport = self.builder.get_object("doneViewport");
         self.doneVBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=1)
-        f = open("lists/done.json", "r");
-        done = json.loads(f.read())
+        with open("lists/done.json", "r") as f:
+            done = json.loads(f.read())
         for fudo in done:
             button = Gtk.Button(fudo[0]);
             button.connect("clicked", self.onDoneClick);
@@ -55,8 +55,8 @@ class Fudo:
         self.addFudo = self.builder.get_object("addFudoDialog")
         self.addFudo.run()
     def onSubmit(self, *args):
-        f = open("lists/todo.json", "r")
-        data = json.loads(f.read())
+        with open("lists/todo.json", "r") as f:
+            data = json.loads(f.read())
         NewFudoName = self.builder.get_object("NameTextInput");
         NewFudoNameText = NewFudoName.get_text()
         NewFudoName.set_text("")
@@ -69,8 +69,8 @@ class Fudo:
         button = Gtk.Button(NewFudoNameText)
         button.connect("clicked", self.onToDoClick)
         self.todoVBox.add(button)
-        f = open("lists/todo.json", "w")
-        f.write(json.dumps(data, indent=4))
+        with open("lists/todo.json", "w") as f:
+            f.write(json.dumps(data, indent=4))
         self.window.show_all()
         self.addFudo.hide()
     def onCancel(self, *args):
@@ -83,8 +83,8 @@ class Fudo:
         self.addFudo.hide()
     def onToDoClick(self, *args):
         self.Fudo = self.builder.get_object("fudoDialog");
-        f = open("lists/todo.json", "r")
-        data = json.loads(f.read())
+        with open("lists/todo.json", "r") as f:
+            data = json.loads(f.read())
         for fudo in data:
             if fudo[0] == args[0].get_label():
                 name = self.builder.get_object("FudoName");
@@ -101,20 +101,20 @@ class Fudo:
                 progress.set_buffer(buf);
         self.Fudo.run();
     def onDoingClick(self, *args):
-        self.Fudo = self.builder.get_object("fudoDialog");
-        f = open("lists/doing.json", "r")
-        data = json.loads(f.read())
+        self.Fudo = self.builder.get_object("fudoDoingDialog");
+        with open("lists/doing.json", "r") as f:
+            data = json.loads(f.read())
         for fudo in data:
             if fudo[0] == args[0].get_label():
-                name = self.builder.get_object("FudoName");
+                name = self.builder.get_object("FudoDoingName");
                 buf = name.get_buffer();
                 buf.set_text(fudo[0]);
                 name.set_buffer(buf);
-                description = self.builder.get_object("FudoDescription");
+                description = self.builder.get_object("FudoDoingDescription");
                 buf = description.get_buffer();
                 buf.set_text(fudo[1]);
                 description.set_buffer(buf);
-                progress = self.builder.get_object("FudoProgress");
+                progress = self.builder.get_object("FudoDoingProgress");
                 buf = progress.get_buffer();
                 seconds = int(time.time() - fudo[2]);
                 minutes, seconds = divmod(seconds, 60);
@@ -124,8 +124,8 @@ class Fudo:
         self.Fudo.run();
     def onDoneClick(self, *args):
         self.Fudo = self.builder.get_object("fudoDoneDialog");
-        f = open("lists/done.json", "r")
-        data = json.loads(f.read())
+        with open("lists/done.json", "r") as f:
+            data = json.loads(f.read())
         for fudo in data:
             if fudo[0] == args[0].get_label():
                 name = self.builder.get_object("FudoDoneName");
@@ -145,48 +145,48 @@ class Fudo:
                 progress.set_buffer(buf);
         self.Fudo.run();
     def onFudoComplete(self, *args):
-        f = open("lists/doing.json", "r");
-        doing = json.loads(f.read());
-        name = self.builder.get_object("FudoName").get_buffer()
+        with open("lists/doing.json", "r") as f:
+            doing = json.loads(f.read());
+        name = self.builder.get_object("FudoDoingName").get_buffer()
         name = name.get_text(name.get_start_iter(), name.get_end_iter(), True);
         for x in range(0, len(doing)):
             if doing[x][0] == name:
-                f = open("lists/done.json", "r");
-                done = json.loads(f.read());
-                done.append([doing[x][0], doing[x][1], (time.time() - doing[x][2])]);
-                f = open("lists/done.json", "w");
-                f.write(json.dumps(done, indent=4));
-                doing.pop(x);
+                with open("lists/done.json", "r") as fr:
+                    done = json.loads(fr.read());
+                    done.append([doing[x][0], doing[x][1], (time.time() - doing[x][2])]);
+                with open("lists/done.json", "w") as fw:
+                    fw.write(json.dumps(done, indent=4));
+                    doing.pop(x);
                 break;
-        f = open("lists/doing.json", "w");
-        if len(doing) > 0:
-            f.write(json.dumps(doing, indent=4));
-        else:
-            f.write("[]");
+        with open("lists/doing.json", "w") as fw:
+            if len(doing) > 0:
+                fw.write(json.dumps(doing, indent=4));
+            else:
+                fw.write("[]");
         self.loadMainData();
         self.window.show_all()
         self.Fudo.hide();
     def onFudoPause(self, *args):
         return 0;
     def onFudoStart(self, *args):
-        f = open("lists/todo.json", "r");
-        data = json.loads(f.read());
+        with open("lists/todo.json", "r") as f:
+            data = json.loads(f.read());
         name = self.builder.get_object("FudoName").get_buffer()
         name = name.get_text(name.get_start_iter(), name.get_end_iter(), True);
         for x in range(0, len(data)):
             if data[x][0] == name:
-                f = open("lists/doing.json", "r");
-                doing = json.loads(f.read());
-                doing.append([data[x][0], data[x][1], time.time()]);
-                f = open("lists/doing.json", "w");
-                f.write(json.dumps(doing, indent=4));
-                data.pop(x);
+                with open("lists/doing.json", "r") as fr:
+                    doing = json.loads(fr.read());
+                    doing.append([data[x][0], data[x][1], time.time()]);
+                with open("lists/doing.json", "w") as fw:
+                    fw.write(json.dumps(doing, indent=4));
+                    data.pop(x);
                 break;
-        f = open("lists/todo.json", "w");
-        if len(data) > 0:
-            f.write(json.dumps(data, indent=4));
-        else:
-            f.write("[]");
+        with open("lists/todo.json", "w") as fr:
+            if len(data) > 0:
+                fr.write(json.dumps(data, indent=4));
+            else:
+                fr.write("[]");
         self.loadMainData();
         self.window.show_all()
         self.Fudo.hide();
